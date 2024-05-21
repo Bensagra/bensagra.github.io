ca3ac9d08d19ea081435a6930451b283821e05a9
Bensagra <Bensagra@gmail.com>
2024-05-20 21:17:18 -0300

    camisas

--- script.js
+++ script.js
@@ -7,11 +7,19 @@ let contacto = document.getElementById("contacto");
 let hombre = document.getElementById("hombre");
 let mujer = document.getElementById("mujer");
 let navegacion = document.getElementById("navegacion");
+let camisasHtml = document.getElementById("ropa");
+
 let ul_document = [
 camisas,sastreria,pantalones,hombre,mujer,sobreNosotros,contacto,
 ]
+let camisas_tabla = [
+    ["Camisa de gamuza","20.000","descripcion","/images/remera.jfif"],
+    ["Camisa de gamuza","20.000","descripcion","/images/remera.jfif"],
+    ["Camisa de gamuza","20.000","descripcion","/images/remera.jfif"],
+  
+];
 
-
+cargarCamisas();
 let tabla = [
     //camisas
     ["Camisas",],
@@ -66,5 +74,41 @@ function asignar(i,j, li){
             a.innerText = tabla[i][j];
             element.appendChild(a);
             li.appendChild(element);
+}
+
+
+function cargarCamisas(){
+    //<div class="fila1">
+      //  <div class="ropa">
+        //    <img src="/images/remera.jfif" alt="" class="ropa-image">
+          //  <div class="ropa-texto">
+            //    <p class="ropa-title">Remera oversize</p>
+              //  <p>20.000 pesos</p>
+           // </div>
+           let div = document.createElement('div');
+           div.classList.add('fila1');
+           for (let i = 0; i < camisas_tabla.length; i++) {
+           
+            let div2 = document.createElement('div');
+            div2.classList.add('ropa');
+            let img = document.createElement('img');
+                img.src = camisas_tabla[i][3];
+                img.classList.add('ropa-image');
+                let div3 = document.createElement('div');
+                div3.classList.add('ropa-texto');
+                let p1 = document.createElement('p');
+                p1.classList.add('ropa-title');
+                p1.innerText = camisas_tabla[i][0];
+                let p2 = document.createElement('p');
+                p2.innerText = camisas_tabla[i][1];
+                div3.appendChild(p1);
+                div3.appendChild(p2);
+                div2.appendChild(img);
+                div2.appendChild(div3);
+                div.appendChild(div2);
+                camisasHtml.appendChild(div);
+
+          
+           }
 }
 
