bdac4086208ae5e5884006d7e26531df5d6342af
Bensagra <Bensagra@gmail.com>
2024-05-21 22:07:22 -0300

    prototipo

---
+++ camisas.js
@@ -0,0 +1,43 @@
+
+function cargarCamisas(){
+    //<div class="fila1">
+      //  <div class="ropa">
+        //    <img src="/images/remera.jfif" alt="" class="ropa-image">
+          //  <div class="ropa-texto">
+            //    <p class="ropa-title">Remera oversize</p>
+              //  <p>20.000 pesos</p>
+           // </div>
+           let div = document.createElement('div');
+           div.classList.add('fila1');
+           for (let i = 0; i < camisas_tabla.length; i++) {
+           
+            let div2 = document.createElement('div');
+            div2.classList.add('ropa');
+            let divImagen = document.createElement('div');
+            divImagen.classList.add('ropa-image');
+            let img = document.createElement('img');
+                img.src = camisas_tabla[i][3];
+                img.classList.add('imagen');
+                divImagen.appendChild(img);
+                let div3 = document.createElement('div');
+                div3.classList.add('ropa-texto');
+                let p1 = document.createElement('p');
+                p1.classList.add('ropa-title');
+                p1.innerText = camisas_tabla[i][0];
+                let p2 = document.createElement('p');
+                p2.innerText = camisas_tabla[i][1];
+                let p3 = document.createElement('p');
+                p3.classList.add('tela');
+                p3.innerText = camisas_tabla[i][4];
+                div3.appendChild(p1);
+                div3.appendChild(p3);
+                div3.appendChild(p2);
+                div2.appendChild(divImagen);
+                div2.appendChild(div3);
+                div.appendChild(div2);
+                camisasHtml.appendChild(div);
+
+          
+           }
+}
+
--- script.js
+++ script.js
@@ -14,9 +14,9 @@ let ul_document = [
 camisas,sastreria,pantalones,hombre,mujer,sobreNosotros,contacto,
 ]
 let camisas_tabla = [
-    ["Camisa de gamuza","20.000","descripcion","/images/remera.jfif"],
-    ["Camisa de gamuza","20.000","descripcion","/images/remera.jfif"],
-    ["Camisa de gamuza","20.000","descripcion","/images/remera.jfif"],
+    ["Camisa","$20,000","descripcion","/images/remera.png","Gamuza"],
+    ["Camisa de gamuza","$20,000","descripcion","/images/remera.png","Gamuza"],
+    ["Camisa de gamuza","$20,000","descripcion","/images/remera.png","Gamuza"],
   
 ];
 
@@ -77,39 +77,4 @@ function asignar(i,j, li){
             li.appendChild(element);
 }
 
-
-function cargarCamisas(){
-    //<div class="fila1">
-      //  <div class="ropa">
-        //    <img src="/images/remera.jfif" alt="" class="ropa-image">
-          //  <div class="ropa-texto">
-            //    <p class="ropa-title">Remera oversize</p>
-              //  <p>20.000 pesos</p>
-           // </div>
-           let div = document.createElement('div');
-           div.classList.add('fila1');
-           for (let i = 0; i < camisas_tabla.length; i++) {
-           
-            let div2 = document.createElement('div');
-            div2.classList.add('ropa');
-            let img = document.createElement('img');
-                img.src = camisas_tabla[i][3];
-                img.classList.add('ropa-image');
-                let div3 = document.createElement('div');
-                div3.classList.add('ropa-texto');
-                let p1 = document.createElement('p');
-                p1.classList.add('ropa-title');
-                p1.innerText = camisas_tabla[i][0];
-                let p2 = document.createElement('p');
-                p2.innerText = camisas_tabla[i][1];
-                div3.appendChild(p1);
-                div3.appendChild(p2);
-                div2.appendChild(img);
-                div2.appendChild(div3);
-                div.appendChild(div2);
-                camisasHtml.appendChild(div);
-
-          
-           }
-}
 
