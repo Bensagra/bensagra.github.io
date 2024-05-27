be566e998fd6f632257d4cb1c1effd0305a7fe00
t-czerwiak <timoczerwiak@gmail.com>
2024-05-27 10:35:38 -0300

    pantalones, hombre y mujer, boceto hechos

--- hombre.js
+++ hombre.js
@@ -1,17 +1,17 @@
-let pantalonesHtml = document.getElementById("ropa");
-let pantalones_tabla = [
-  ["Pantalones","$5","descripcion","/images/remera.png","Jean"],
-  ["Pantalones","$5","descripcion","/images/remera.png","Jean"],
-  ["Pantalones","$5","descripcion","/images/remera.png","Jean"],
-  ["Pantalones","$5","descripcion","/images/remera.png","Jean"],
-  ["Pantalones","$5","descripcion","/images/remera.png","Jean"],
-  ["Pantalones","$5","descripcion","/images/remera.png","Jean"],
+let hombreHtml = document.getElementById("ropa");
+let hombre_tabla = [
+  ["hombre","$5","descripcion","/images/remera.png","Jean"],
+  ["hombre","$5","descripcion","/images/remera.png","Jean"],
+  ["hombre","$5","descripcion","/images/remera.png","Jean"],
+  ["hombre","$5","descripcion","/images/remera.png","Jean"],
+  ["hombre","$5","descripcion","/images/remera.png","Jean"],
+  ["hombre","$5","descripcion","/images/remera.png","Jean"],
 ];
 
-function cargarPantalones(){
+function cargarhombre(){
   let div = document.createElement('div');
   div.classList.add('fila1');
-  for (let i = 0; i < pantalones_tabla.length; i++) {
+  for (let i = 0; i < hombre_tabla.length; i++) {
     crearWidget(div,i);
 }
 }
@@ -22,26 +22,26 @@ function crearWidget(messi,i){
   let divImagen = document.createElement('div');
   divImagen.classList.add('ropa-image');
   let img = document.createElement('img');
-      img.src = pantalones_tabla[i][3];
+      img.src = hombre_tabla[i][3];
       img.classList.add('imagen');
       divImagen.appendChild(img);
       let div3 = document.createElement('div');
       div3.classList.add('ropa-texto');
       let p1 = document.createElement('p');
       p1.classList.add('ropa-title');
-      p1.innerText = pantalones_tabla[i][0];
+      p1.innerText = hombre_tabla[i][0];
       let p2 = document.createElement('p');
-      p2.innerText = pantalones_tabla[i][1];
+      p2.innerText = hombre_tabla[i][1];
       let p3 = document.createElement('p');
       p3.classList.add('tela');
-      p3.innerText = pantalones_tabla[i][4];
+      p3.innerText = hombre_tabla[i][4];
       div3.appendChild(p1);
       div3.appendChild(p3);
       div3.appendChild(p2);
       div2.appendChild(divImagen);
       div2.appendChild(div3);
       messi.appendChild(div2);
-      pantalonesHtml.appendChild(messi);
+      hombreHtml.appendChild(messi);
 
 }
-cargarPantalones();
+cargarhombre();
