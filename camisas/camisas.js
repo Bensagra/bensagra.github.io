c542e62ce1bb66af8d0f3b9351d444bc5b98fc98
bensagra <bensagra@gmail.com>
2024-06-24 11:03:39 -0300

    dmamds

--- camisas/camisas.js
+++ camisas/camisas.js
@@ -1,15 +1,50 @@
 let camisasHtml = document.getElementById("ropa");
 let camisas_tabla = [
-  ["Camisa","$20,000","descripcion","/images/remera.png","Gamuza"],
-  ["Camisa","$20,000","descripcion","/images/remera.png","Gamuza"],
-  ["Camisa","$20,000","descripcion","/images/remera.png","Gamuza"],
-  ["Camisa","$20,000","descripcion","/images/remera.png","Gamuza"],
-  ["Camisa","$20,000","descripcion","/images/remera.png","Gamuza"],
-  ["Camisa","$20,000","descripcion","/images/remera.png","Gamuza"],
-
+ 
 ];
 
+const url = "https://turkey-api-2k7c.vercel.app/api/turkey/get_productos?category_id=1";
+getData();
+async function getData(){
+  const response = await fetch(url,{ });
+  const data = await response.json();
+  console.log(data);
+  for (let i = 0; i < data.length; i++) {
+   
+ 
+    let newProduct ={
+        "product_name" : data[i].product_name,
+        "product_title" : data[i].product_title,
+        "product_description" : data[i].product_description,
+        "product_material_id" : data[i].product_material_id,
+        "product_photo_url" : data[i].product_photo_url,
+        "product_price" : data[i].product_price,
+        "product_subcategory_id" : data[i].product_subcategory_id,
+        "product_gender_id" : data[i].product_gender_id
+    }
+   
+    
+        camisas_tabla.push(newProduct);
+        console.log(camisas_tabla);
+        const other = await fetch("https://turkey-api-2k7c.vercel.app/api/turkey/get_material_name?product_material_id="+ camisas_tabla[i].product_material_id,{
+    
+        });
+        const material = await other.json();
+        console.log(material);
+        let newMaterial ={
+          "material_id" : material[0].material_id,
+          "material_type" : material[0].material_type,
+         
+      }
+        camisas_tabla[i].product_material_id = newMaterial.material_type;
 
+    
+    
+    
+        
+    }
+  cargarCamisas();
+}
 function cargarCamisas(){
            let div = document.createElement('div');
            div.classList.add('fila1');
@@ -24,19 +59,19 @@ function crearWidget(messi,i){
   let divImagen = document.createElement('div');
   divImagen.classList.add('ropa-image');
   let img = document.createElement('img');
-      img.src = camisas_tabla[i][3];
+      img.src = camisas_tabla[i].product_photo_url;
       img.classList.add('imagen');
       divImagen.appendChild(img);
       let div3 = document.createElement('div');
       div3.classList.add('ropa-texto');
       let p1 = document.createElement('p');
       p1.classList.add('ropa-title');
-      p1.innerText = camisas_tabla[i][0];
+      p1.innerText = camisas_tabla[i].product_name;
       let p2 = document.createElement('p');
-      p2.innerText = camisas_tabla[i][1];
+      p2.innerText = camisas_tabla[i].product_title;
       let p3 = document.createElement('p');
       p3.classList.add('tela');
-      p3.innerText = camisas_tabla[i][4];
+      p3.innerText = camisas_tabla[i].product_material_id;
       div3.appendChild(p1);
       div3.appendChild(p3);
       div3.appendChild(p2);
